704d65127f14679566bb64d5de9bc6434a0e026e
Tom Lawton <tom.lawton@cndu.it>
2024-01-13 23:09:49 +0000

    fix: link containers via /proc and /srv

--- openresty/docker/templates/start-nginx.sh
+++ openresty/docker/templates/start-nginx.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-get_php_pid() {
-    echo $(ps | grep "start-php-fpm" -m1 | grep -v grep | sed 's/^[ \t]*//;s/[ \t]*$//' | cut -d " " -f1)
-}
-
-echo "Waiting for php container process to be available in processlist..."
-
-while [ -z "$(get_php_pid)" ]; do sleep 1; done
-
-echo "Found php container process PID: $(get_php_pid)"
-
-ln -s /proc/$(ps | grep "start-php-fpm" -m1 | grep -v grep | sed 's/^[ \t]*//;s/[ \t]*$//' | cut -d " " -f1)/root/app/web $DOCUMENT_ROOT
-
-echo "Linked cross-process webroot"
-
-
-
+rm -f $DOCUMENT_ROOT
+ln -s /srv/web $DOCUMENT_ROOT
 
 /usr/bin/openresty -g 'daemon off;' -c /usr/local/docker/etc/nginx/nginx.conf
